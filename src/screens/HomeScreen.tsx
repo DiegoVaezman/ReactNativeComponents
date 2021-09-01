30a7d32aff7f3d0abe82b80544a0e56db9c5c7eb
DiegoVaezman <diego.ng90@gmail.com>
2021-09-01 18:06:16 +0200

    first instalations/dependencies React Native

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+
+export const HomeScreen = () => {
+
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+            <Icon name="cloudy-night-outline" size={300}/>
+        </View>
+    )
+}
